b80e18e286b09487028daddc10dead5df30aff6a
Ba-Yint-Naung <bayintnaung9876@gmail.com>
2020-05-13 23:16:30 +0630

    Frist Commit

--- app/src/main/java/com/bayintnaung/clientapp/AboutFragment.java
+++ app/src/main/java/com/bayintnaung/clientapp/AboutFragment.java
@@ -31,7 +31,7 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        myView = inflater.inflate(R.layout.fragment_about, container, false);
+        myView = inflater.inflate(R.layout.fragment_about, container,false);
         GoogleAds googleAds=new GoogleAds();
         googleAds.loadAdVerticalAds(myView,getContext(),getActivity());
         return myView;
